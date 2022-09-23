f92b4ac2f75ac1e50ae6ff029668472f77ba0a1c
king-odysseus <wingzatari@gmail.com>
2022-09-23 16:50:19 +0100

    app-chat send button

--- resources/views/app-chat.blade.php
+++ resources/views/app-chat.blade.php
@@ -338,7 +338,7 @@ Dessert chocolate cake lemon drops jujubes. Biscuit cupcake ice cream bear claw 
                                     <div class="chat chat-left">
                                         <div class="chat-avatar">
                                             <span class="avatar box-shadow-1 cursor-pointer">
-                                                <img src="../../../app-assets/images/portrait/small/avatar.jpg" alt="avatar" height="36" width="36" />
+                                                <img src="../../../app-assets/images/portrait/small/avatarjpg" alt="avatar" height="36" width="36" />
                                             </span>
                                         </div>
                                         <div class="chat-body">
@@ -361,107 +361,107 @@ Dessert chocolate cake lemon drops jujubes. Biscuit cupcake ice cream bear claw 
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- User Chat messages -->
 
-                        <!-- Submit Chat form -->
-                        <form class="chat-app-form" action="javascript:void(0);" onsubmit="enterChat();">
-                            <div class="input-group input-group-merge me-1 form-send-message">
-                                <span class="speech-to-text input-group-text"><i data-feather="mic" class="cursor-pointer"></i></span>
-                                <input type="text" class="form-control message" placeholder="Type your message or use speech to text" />
-                                <span class="input-group-text">
-                                    <label for="attach-doc" class="attachment-icon form-label mb-0">
-                                        <i data-feather="image" class="cursor-pointer text-secondary"></i>
-                                        <input type="file" id="attach-doc" hidden /> </label></span>
+                            <!-- User Chat messages -->
+
+                            <!-- Submit Chat form -->
+                            <form class="chat-app-form" action="javascript:void(0);" onsubmit="enterChat();">
+                                <div class="input-group input-group-merge me-1 form-send-message">
+                                    <span class="speech-to-text input-group-text"><i data-feather="mic" class="cursor-pointer"></i></span>
+                                    <input type="text" class="form-control message" placeholder="Type your message or use speech to text" />
+                                    <span class="input-group-text">
+                                        <label for="attach-doc" class="attachment-icon form-label mb-0">
+                                            <i data-feather="image" class="cursor-pointer text-secondary"></i>
+                                            <input type="file" id="attach-doc" hidden /> </label></span>
+                                </div>
+                                <button type="button" class="btn btn-primary send" onclick="enterChat();">
+                                    <i data-feather="send" class="d-lg-none"></i>
+                                    <span class="d-none d-lg-block">Send</span>
+                                </button>
+                            </form>
+                            <!--/ Submit Chat form -->
+                        </div>
+                        <!--/ Active Chat -->
+                    </section>
+                    <!--/ Main chat area -->
+
+                    <!-- User Chat profile right area -->
+                    <div class="user-profile-sidebar">
+                        <header class="user-profile-header">
+                            <span class="close-icon">
+                                <i data-feather="x"></i>
+                            </span>
+                            <!-- User Profile image with name -->
+                            <div class="header-profile-sidebar">
+                                <div class="avatar box-shadow-1 avatar-border avatar-xl">
+                                    <img src="../../../app-assets/images/portrait/small/avatar-s-7.jpg" alt="user_avatar" height="70" width="70" />
+                                    <span class="avatar-status-busy avatar-status-lg"></span>
+                                </div>
+                                <h4 class="chat-user-name">Kristopher Candy</h4>
+                                <span class="user-post">UI/UX Designer 👩🏻‍💻</span>
                             </div>
-                            <button type="button" class="btn btn-primary send" onclick="enterChat();">
-                                <i data-feather="send" class="d-lg-none"></i>
-                                <span class="d-none d-lg-block">Send</span>
-                            </button>
-                        </form>
-                        <!--/ Submit Chat form -->
-                </div>
-                <!--/ Active Chat -->
-                </section>
-                <!--/ Main chat area -->
-
-                <!-- User Chat profile right area -->
-                <div class="user-profile-sidebar">
-                    <header class="user-profile-header">
-                        <span class="close-icon">
-                            <i data-feather="x"></i>
-                        </span>
-                        <!-- User Profile image with name -->
-                        <div class="header-profile-sidebar">
-                            <div class="avatar box-shadow-1 avatar-border avatar-xl">
-                                <img src="../../../app-assets/images/portrait/small/avatar-s-7.jpg" alt="user_avatar" height="70" width="70" />
-                                <span class="avatar-status-busy avatar-status-lg"></span>
+                            <!--/ User Profile image with name -->
+                        </header>
+                        <div class="user-profile-sidebar-area">
+                            <!-- About User -->
+                            <h6 class="section-label mb-1">About</h6>
+                            <p>Toffee caramels jelly-o tart gummi bears cake I love ice cream lollipop.</p>
+                            <!-- About User -->
+                            <!-- User's personal information -->
+                            <div class="personal-info">
+                                <h6 class="section-label mb-1 mt-3">Personal Information</h6>
+                                <ul class="list-unstyled">
+                                    <li class="mb-1">
+                                        <i data-feather="mail" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">kristycandy@email.com</span>
+                                    </li>
+                                    <li class="mb-1">
+                                        <i data-feather="phone-call" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">+1(123) 456 - 7890</span>
+                                    </li>
+                                    <li>
+                                        <i data-feather="clock" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Mon - Fri 10AM - 8PM</span>
+                                    </li>
+                                </ul>
                             </div>
-                            <h4 class="chat-user-name">Kristopher Candy</h4>
-                            <span class="user-post">UI/UX Designer 👩🏻‍💻</span>
-                        </div>
-                        <!--/ User Profile image with name -->
-                    </header>
-                    <div class="user-profile-sidebar-area">
-                        <!-- About User -->
-                        <h6 class="section-label mb-1">About</h6>
-                        <p>Toffee caramels jelly-o tart gummi bears cake I love ice cream lollipop.</p>
-                        <!-- About User -->
-                        <!-- User's personal information -->
-                        <div class="personal-info">
-                            <h6 class="section-label mb-1 mt-3">Personal Information</h6>
-                            <ul class="list-unstyled">
-                                <li class="mb-1">
-                                    <i data-feather="mail" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">kristycandy@email.com</span>
-                                </li>
-                                <li class="mb-1">
-                                    <i data-feather="phone-call" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">+1(123) 456 - 7890</span>
-                                </li>
-                                <li>
-                                    <i data-feather="clock" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Mon - Fri 10AM - 8PM</span>
-                                </li>
-                            </ul>
-                        </div>
-                        <!--/ User's personal information -->
+                            <!--/ User's personal information -->
 
-                        <!-- User's Links -->
-                        <div class="more-options">
-                            <h6 class="section-label mb-1 mt-3">Options</h6>
-                            <ul class="list-unstyled">
-                                <li class="cursor-pointer mb-1">
-                                    <i data-feather="tag" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Add Tag</span>
-                                </li>
-                                <li class="cursor-pointer mb-1">
-                                    <i data-feather="star" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Important Contact</span>
-                                </li>
-                                <li class="cursor-pointer mb-1">
-                                    <i data-feather="image" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Shared Media</span>
-                                </li>
-                                <li class="cursor-pointer mb-1">
-                                    <i data-feather="trash" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Delete Contact</span>
-                                </li>
-                                <li class="cursor-pointer">
-                                    <i data-feather="slash" class="font-medium-2 me-50"></i>
-                                    <span class="align-middle">Block Contact</span>
-                                </li>
-                            </ul>
+                            <!-- User's Links -->
+                            <div class="more-options">
+                                <h6 class="section-label mb-1 mt-3">Options</h6>
+                                <ul class="list-unstyled">
+                                    <li class="cursor-pointer mb-1">
+                                        <i data-feather="tag" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Add Tag</span>
+                                    </li>
+                                    <li class="cursor-pointer mb-1">
+                                        <i data-feather="star" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Important Contact</span>
+                                    </li>
+                                    <li class="cursor-pointer mb-1">
+                                        <i data-feather="image" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Shared Media</span>
+                                    </li>
+                                    <li class="cursor-pointer mb-1">
+                                        <i data-feather="trash" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Delete Contact</span>
+                                    </li>
+                                    <li class="cursor-pointer">
+                                        <i data-feather="slash" class="font-medium-2 me-50"></i>
+                                        <span class="align-middle">Block Contact</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--/ User's Links -->
                         </div>
-                        <!--/ User's Links -->
                     </div>
-                </div>
-                <!--/ User Chat profile right area -->
+                    <!--/ User Chat profile right area -->
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 <!-- END: Content-->
 
